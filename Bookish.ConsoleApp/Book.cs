50774d0c67059fc9accf2ef248deb6a543d319c8
Mikey Teal <Mikey.Teal@softwire.com>
2017-11-10 11:56:08 +0000

    Created a list of all books in the database

---
+++ Bookish.ConsoleApp/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookish.ConsoleApp
+{
+    public class Book
+    {
+        public int book_id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string isbn { get; set; }
+        public bool available { get; set; }
+    }
+}
--- Bookish.ConsoleApp/Program.cs
+++ Bookish.ConsoleApp/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace Bookish.ConsoleApp
 {
@@ -10,6 +14,22 @@ namespace Bookish.ConsoleApp
     {
         static void Main(string[] args)
         {
+            IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["Bookish"].ConnectionString);
+
+            string SqlString = "SELECT TOP 100 [book_id],[title],[author],[isbn],[available] FROM [books]";
+            var ourBooks = (List<Book>)db.Query<Book>(SqlString);
+
+            foreach (var books in ourBooks)
+            {
+                Console.WriteLine(new string('*', 20));
+                Console.WriteLine("\nBook ID: " + books.book_id);
+                Console.WriteLine("Title: " + books.title);
+                Console.WriteLine("Author: " + books.author);
+                Console.WriteLine("Is available? " + books.available + "\n");
+                Console.WriteLine(new string('*', 20));
+            }
+
+            Console.ReadLine();
         }
     }
 }
